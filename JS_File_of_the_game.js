7b435e242b8825cb97e797318892a6e6121f9ba1
MykhayloVoroshylov <161176411+MykhayloVoroshylov@users.noreply.github.com>
2024-08-25 00:45:15 -0400

    Update JS_File_of_the_game.js
    Made the result work with HTML file

--- JS_File_of_the_game.js
+++ JS_File_of_the_game.js
@@ -6,13 +6,19 @@ function Choice1() {
     let computer = Math.floor(Math.random() * (max - min) + min);
     console.log(computer);
     if (computer==1){
-        console.log("Tie!")
+        console.log("Tie!");
+        var Result = document.getElementById("Result").value="Tie"
+        document.getElementById("Result").innerHTML = Result
     }
     else if (computer==2){
-        console.log("Computer Won!")
+        console.log("Computer Won!");
+        var Result = document.getElementById("Result").value="Computer Won!" 
+        document.getElementById("Result").innerHTML = Result
     }
     else {
-        console.log("Player Won!")
+        console.log("Player Won!");
+        var Result = document.getElementById("Result").value="Player Won!" 
+        document.getElementById("Result").innerHTML = Result
     }
 }
 
@@ -24,13 +30,19 @@ function Choice2() {
     let computer = Math.floor(Math.random() * (max - min) + min);
     console.log(computer);
     if (computer==1){
-        console.log("Player Won!")
+        console.log("Player Won!");
+        var Result = document.getElementById("Result").value="Player Won!" 
+        document.getElementById("Result").innerHTML = Result
     }
     else if (computer==2){
-        console.log("Tie!")
+        console.log("Tie!");
+        var Result = document.getElementById("Result").value="Tie"
+        document.getElementById("Result").innerHTML = Result
     }
     else {
-        console.log("Computer Won!")
+        console.log("Computer Won!");
+        var Result = document.getElementById("Result").value="Computer Won!" 
+        document.getElementById("Result").innerHTML = Result
     }
 }
 
@@ -42,15 +54,18 @@ function Choice3() {
     let computer = Math.floor(Math.random() * (max - min) + min);
     console.log(computer);
     if (computer==1){
-        console.log("Computer Won!")
+        console.log("Computer Won!");
+        var Result = document.getElementById("Result").value="Computer Won!" 
+        document.getElementById("Result").innerHTML = Result
     }
     else if (computer==2){
-        console.log("Player Won!")
+        console.log("Player Won!");
+        var Result = document.getElementById("Result").value="Player Won!" 
+        document.getElementById("Result").innerHTML = Result
     }
     else {
-        console.log("Tie!")
+        console.log("Tie!");
+        var Result = document.getElementById("Result").value="Tie"
+        document.getElementById("Result").innerHTML = Result
     }
 }
-
-
-
